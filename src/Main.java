0f1fa15584f34c8813a72eb677663130ce4097b8
Kirill Machnev <machnev999@gmail.com>
2021-09-01 20:52:55 +0300

    inintial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 200;
+        int mille = price / 20;
+        int Bonus = mille;
+
+        System.out.println(Bonus);
+
+    }
+}
